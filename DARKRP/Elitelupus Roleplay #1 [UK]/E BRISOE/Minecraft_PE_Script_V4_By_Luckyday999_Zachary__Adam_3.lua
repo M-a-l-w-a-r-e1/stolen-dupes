6938ea607f860488106b334d440033078f327f2b
M-a-l-w-a-r-e1 <71583406+M-a-l-w-a-r-e1@users.noreply.github.com>
2022-10-29 09:45:00 -0400

    Add files via upload

---
+++ DARKRP/Elitelupus Roleplay #1 [UK]/E BRISOE/Minecraft_PE_Script_V4_By_Luckyday999_Zachary__Adam_3.lua
@@ -0,0 +1,400 @@
+if gg.isVisible(true) 
+  then 
+    gg.setVisible(false)
+  end
+
+function main()
+Menu = gg.choice({
+	"Speed Hack ON",
+	"Speed Hack OFF",
+	"Air Walking ON",
+	"Air Walking OFF",
+	"Air Jump ON",
+	"Air Jump OFF",
+	"Jump blocks ON",
+	"Jump Blocks OFF",
+	"No Fall Damage",
+	"Fly Mode",
+	"God Mode",
+	"Fast Sprint",
+	"Enlarged Hit Box ON",
+	"Enlarged Hit Box OFF",
+	"High FOV ON (Sprint While Enabling)",
+	"High FOV OFF (Sprint While Disabling)",
+	"Teleport ON (Turn On Walk & Turn Off & You Will Be Teleported)",
+	"Teleport OFF",
+	"Broken Arm Mode (this effects Mobs)",
+	"Speed Hack V2 ON",
+	"Speed Hack V2 OFF",
+	"Head Rotate ON",
+	"Head Rotate OFF",
+	"Jump Boost ON",
+	"Jump Boost OFF",
+	"INFORMATION",
+	"🚫EXIT🚫"},
+ nil, "Minecraft PE Script V4 By Luckyday999 Zachary Pelayo & Adam Wibu")
+
+if Menu == 1 then F1() end
+if Menu == 2 then F2() end
+if Menu == 3 then F3() end
+if Menu == 4 then F4() end
+if Menu == 5 then F5() end
+if Menu == 6 then F6() end
+if Menu == 7 then F7() end
+if Menu == 8 then F8() end
+if Menu == 9 then F9() end
+if Menu == 10 then F10() end
+if Menu == 11 then F11() end
+if Menu == 12 then F12() end
+if Menu == 13 then F13() end
+if Menu == 14 then F14() end
+if Menu == 15 then F15()end
+if Menu == 16 then F16() end
+if Menu == 17 then F17()end
+if Menu == 18 then F18() end
+if Menu == 19 then F19()end
+if Menu == 20 then F20()end
+if Menu == 21 then F21() end
+if Menu == 22 then F22()end
+if Menu == 23 then F23() end
+if Menu == 24 then F24()end
+if Menu == 25 then F24() end
+if Menu == 26 then F26()end
+if Menu == 27 then Exit() end
+end
+
+function F1()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("0.1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.processResume()
+gg.refineNumber("0.1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.processResume()
+gg.refineNumber("0.1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.processResume()
+gg.refineNumber("0.1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.processResume()
+gg.refineNumber("0.1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.processResume()
+gg.refineNumber("0.1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.searchNumber("0.1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.processResume()
+gg.refineNumber("0.1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.processResume()
+gg.refineNumber("0.1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.processResume()
+gg.refineNumber("0.1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.processResume()
+gg.refineNumber("0.1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.processResume()
+gg.refineNumber("0.1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.searchNumber("0.1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.processResume()
+gg.refineNumber("0.1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.processResume()
+gg.refineNumber("0.1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.processResume()
+gg.refineNumber("0.1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.processResume()
+gg.refineNumber("0.1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.processResume()
+gg.refineNumber("0.1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.searchNumber("0.1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.processResume()
+gg.refineNumber("0.1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.processResume()
+gg.refineNumber("0.1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.processResume()
+gg.refineNumber("0.1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.processResume()
+gg.refineNumber("0.1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.processResume()
+gg.refineNumber("0.1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.searchNumber("0.1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.processResume()
+gg.refineNumber("0.1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.processResume()
+gg.refineNumber("0.1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.processResume()
+gg.refineNumber("0.1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.processResume()
+gg.refineNumber("0.1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.processResume()
+gg.refineNumber("0.1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.searchNumber("0.1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.processResume()
+gg.refineNumber("0.1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.processResume()
+gg.refineNumber("0.1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.processResume()
+gg.refineNumber("0.1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.processResume()
+gg.refineNumber("0.1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.processResume()
+gg.refineNumber("0.1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+revert = gg.getResults(5000, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.editAll("0.85200000149", gg.TYPE_FLOAT)
+gg.refineNumber("99999999999999", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.toast('⚔️Speed Hack ON⚔️')
+end
+
+function F2()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("0.85200000149", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+
+revert = gg.getResults(5000, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.editAll("0.1", gg.TYPE_FLOAT)
+gg.refineNumber("99999999999999", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.toast('⚔️Speed Hack OFF⚔️')
+end
+
+function F3()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("-0.07840000093", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+
+revert = gg.getResults(5000, nil, nil, nil, nil, nil, nil, nil, nil)
+local t = gg.getResults(5000, nil, nil, nil, nil, nil, nil, nil, nil)
+for i, v in ipairs(t) do
+	if v.flags == gg.TYPE_FLOAT then
+		v.value = "0"
+		v.freeze = true
+	end
+end
+gg.addListItems(t)
+t = nil
+gg.refineNumber("-99999999999999", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.toast('⚔️Air Walking ON⚔️')
+end
+
+function F4()
+revert = gg.getListItems()
+local t = gg.getListItems()
+for i, v in ipairs(t) do
+	if v.flags == gg.TYPE_FLOAT then
+		v.value = "-0.07840000093"
+		v.freeze = false
+		v.freezeType = gg.FREEZE_NORMAL
+	end
+end
+gg.addListItems(t)
+t = nil
+
+if revert ~= nil then gg.setValues(revert) end
+gg.processResume()
+gg.refineNumber("-99999999999999", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.toast('⚔️Air Walking OFF⚔️')
+end
+function F5()
+gg.searchNumber("16777473D;0.5625::81", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.processResume()
+gg.refineNumber("16777473", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1, 0)
+
+revert = gg.getResults(5000, nil, nil, nil, nil, nil, nil, nil, nil)
+local t = gg.getResults(5000, nil, nil, nil, nil, nil, nil, nil, nil)
+for i, v in ipairs(t) do
+	if v.flags == gg.TYPE_DWORD then
+		v.value = "16777473"
+		v.freeze = true
+	end
+end
+gg.addListItems(t)
+t = nil
+
+gg.refineNumber("999999999", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1, 0)
+t = nil
+gg.toast('⚔️Air Jump ON⚔️')
+end
+function F6()
+revert = gg.getListItems()
+local t = gg.getListItems()
+for i, v in ipairs(t) do
+	if v.flags == gg.TYPE_DWORD then
+		v.value = "16777473"
+		v.freeze = false
+		v.freezeType = gg.FREEZE_NORMAL
+	end
+end
+gg.addListItems(t)
+t = nil
+gg.toast('⚔️Air Jump OFF⚔️')
+end
+function F7()
+gg.searchNumber("0.5625", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.processResume()
+revert = gg.getResults(5000, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.editAll("999.555", gg.TYPE_FLOAT)
+gg.refineNumber("99999999999999", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.toast('⚔️Jump Blocks ON⚔️')
+end
+function F8()
+gg.searchNumber("999.555", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.processResume()
+revert = gg.getResults(5000, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.editAll("0.5625", gg.TYPE_FLOAT)
+gg.refineNumber("99999999999999", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.toast('⚔️Jump Blocks OFF⚔️')
+end
+
+function F9()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("0F;16777473D::5", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.refineNumber("0", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+
+revert = gg.getResults(5000, nil, nil, nil, nil, nil, nil, nil, nil)
+local t = gg.getResults(5000, nil, nil, nil, nil, nil, nil, nil, nil)
+for i, v in ipairs(t) do
+	if v.flags == gg.TYPE_FLOAT then
+		v.value = "0.0"
+		v.freeze = true
+	end
+end
+gg.addListItems(t)
+t = nil
+
+gg.refineNumber("999999999", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.toast('⚔️No Fall Damage ON⚔️')
+end
+function F10()
+gg.searchNumber("0B;0.05000000075F;0.10000000149F::49", gg.TYPE_BYTE, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.processResume()
+revert = gg.getResults(5000, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.editAll("1", gg.TYPE_BYTE)
+gg.refineNumber("99", gg.TYPE_BYTE, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.toast('⚔️Flying ON⚔️')
+end
+function F11()
+gg.searchNumber("0B;0.05000000075F;0.10000000149F::73", gg.TYPE_BYTE, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.searchNumber("0", gg.TYPE_BYTE, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.processResume()
+revert = gg.getResults(5000, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.editAll("1", gg.TYPE_BYTE)
+gg.refineNumber("99", gg.TYPE_BYTE, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.toast('⚔️God Mode ON⚔️')
+end
+function F12()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("9 640 188 314", gg.TYPE_QWORD, false, gg.SIGN_EQUAL, 0, -1, 0)
+revert = gg.getResults(5000, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.editAll("9666 088141", gg.TYPE_QWORD)
+gg.toast('⚔️Fast Sprint ON⚔️')
+end
+
+function F13()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("4 604 480 258 364 250 522", gg.TYPE_QWORD, false, gg.SIGN_EQUAL, 0, -1, 0)
+revert = gg.getResults(5000, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.editAll("4 604 480 258 398 224 384", gg.TYPE_QWORD)
+gg.refineNumber("999999999", gg.TYPE_QWORD, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.toast('⚔️Enlarged Hit Box ON⚔️')
+end
+function F14()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("4 604 480 258 398 224 384", gg.TYPE_QWORD, false, gg.SIGN_EQUAL, 0, -1, 0)
+revert = gg.getResults(5000, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.editAll("4 604 480 258 364 250 522", gg.TYPE_QWORD)
+gg.refineNumber("999999999", gg.TYPE_QWORD, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.toast('⚔️Enlarged Hit Box OFF⚔️')
+end
+function F15()
+gg.searchNumber('60', gg.TYPE_FLOAT)
+gg.getResults(500)
+print('Replaced: ', gg.editAll('500', gg.TYPE_FLOAT))
+gg.clearResults()
+gg.toast('⚔️High FOV ON⚔️')
+end
+function F16()
+gg.searchNumber('500', gg.TYPE_FLOAT)
+gg.getResults(500)
+print('Replaced: ', gg.editAll('60', gg.TYPE_FLOAT))
+gg.clearResults()
+gg.toast('⚔️High FOV OFF⚔️')
+end
+function F17()
+gg.clearResults()
+gg.searchNumber("0.1", gg.TYPE_FLOAT)
+gg.getResults(1000)
+print('Replaced: ', gg.editAll("1000", gg.TYPE_FLOAT))
+gg.toast('⚔️Teleport ON⚔️')
+end
+function F18()
+gg.searchNumber("1000", gg.TYPE_FLOAT)
+gg.getResults(1000)
+print('Replaced: ', gg.editAll("0.1", gg.TYPE_FLOAT))
+gg.toast('⚔️Teleport OFF⚔️')
+end
+function F19()
+gg.setRanges(gg.REGION_JAVA_HEAP | gg.REGION_C_HEAP | gg.REGION_C_ALLOC | gg.REGION_C_DATA | gg.REGION_C_BSS | gg.REGION_PPSSPP)
+gg.searchNumber('-5', gg.TYPE_FLOAT)
+gg.getResults(6666666666)
+print('Replaced: ', gg.editAll('49.94', gg.TYPE_FLOAT))
+gg.toast('⚔️Broken Arm ON⚔️')
+end
+function F20()
+gg.setSpeed(3)
+gg.toast('⚔️Speed Hack V2 ON⚔️')
+end
+function F21()
+gg.setSpeed(1)
+gg.toast('⚔️Speed Hack V2 OFF⚔️')
+end
+
+function F22()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber(" 1.5690510273 ", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0.6)
+gg.getResults(2500)
+gg.editAll(" 999991119 ", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.searchNumber(" -1.5690510273 ", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0.6)
+gg.getResults(2500)
+gg.editAll(" -9999911199 ", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.toast('⚔️Head Rotate ON⚔️')
+end
+function F23()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber(" 999991119 ", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0.6)
+gg.getResults(2500)
+gg.editAll("1.5690510273", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.searchNumber(" -9999911199 ", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0.6)
+gg.getResults(2500)
+gg.editAll(" -1.5690510273 ", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.toast('⚔️Head Rotate OFF⚔️')
+end
+function F24()
+gg.setRanges(gg.REGION_CODE_APP)
+gg.searchNumber(" -0.07999999821 ", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0.6)
+gg.getResults(100)
+gg.editAll(" -0.02999999821 ", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.toast('⚔️Jump Boost ON⚔️')
+end
+function F25()
+gg.setRanges(gg.REGION_CODE_APP)
+gg.searchNumber(" -0.02999999821 ", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0.6)
+gg.getResults(100)
+gg.editAll(" -0.07999999821 ", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.toast('⚔️Jump Boost OFF⚔️')
+end
+function F26()
+gg.clearResults()
+gg.alert('Thanks you for using the script Some Options May Need To Be Enabled Twice Or More To Get A Result - Big Thanks to Zachary Pelayo & Adam Wibu Or This Would Not Be Possible They Have Both Helped Me Alot So Go Subscribe To there channels 1st Channel Zachary Pelayo 2nd Channel Adam Wibu')
+end
+
+function Exit()
+gg.toast('🚫You have Exited The Script🚫')
+gg.clearResults()
+os.exit()
+end
+
+main()
+
+while(true)
+do
+  while gg.isVisible(true)
+    do
+      gg.setVisible(false)
+      main()
+    end
+end
